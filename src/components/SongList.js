08d83a974d1f25df1d15d3cc0708e4b9ed98d996
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-08 16:02:29 -0500

    Map over songs and add button

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Songlist extends Component {
+    
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            )
+        })
+    } 
+
     render() {
         return (
-            <div>
-                Song List
+            <div className="ui divided list">
+                {this.renderList()}
             </div>
         );
     }
